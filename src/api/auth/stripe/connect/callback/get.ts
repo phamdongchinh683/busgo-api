6bc300facd7ae62596b578be25f7c7468a8a702b
pham dong chinh <dchinh6803@gmail.com>
2026-04-18 19:12:54 +0700

    fixed type

--- src/api/auth/stripe/connect/callback/get.ts
+++ src/api/auth/stripe/connect/callback/get.ts
@@ -2,6 +2,7 @@ import { api, endpoint, tags, bearer } from '../../../../../app/api.js'
 import { requiredAuthenticate } from '../../../../../app/jwt/handler.js'
 import { bus } from '../../../../../business/index.js'
 import { MessageResponse } from '../../../../../model/common.js'
+import { StripeStatusReponse } from '../../../../../service/stripe/type.js'
 
 const __filename = new URL('', import.meta.url).pathname
 
@@ -14,7 +15,7 @@ api.route({
     },
 
     schema: {
-        response: { 200: MessageResponse },
+        response: { 200: StripeStatusReponse },
         tags: tags(__filename),
         security: bearer,
     },
