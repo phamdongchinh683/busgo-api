8143992766332f2b3b3cfaf2f1fe9bf9a59eaeff
pham dong chinh <dchinh6803@gmail.com>
2026-04-20 20:37:25 +0700

    fixed time access token 20-4-2026

--- src/app/jwt/auth/handler.ts
+++ src/app/jwt/auth/handler.ts
@@ -6,7 +6,7 @@ import { db } from '../../../datasource/db.js'
 
 const sign = createSigner({
     algorithm: 'HS256',
-    expiresIn: `7 days`,
+    expiresIn: `1h`,
     key: process.env.JWT_SECRET,
 })
 const verify = createVerifier({ key: process.env.JWT_SECRET })
