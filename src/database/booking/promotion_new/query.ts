7a05cfd235f8d6854b686790082e3e121c87d52f
pham dong chinh <dchinh6803@gmail.com>
2026-05-08 16:40:42 +0700

    ...

--- src/database/booking/promotion_new/query.ts
+++ src/database/booking/promotion_new/query.ts
@@ -1,17 +1,17 @@
 import { db } from '../../../datasource/db.js'
 import { PromotionNewsListQuery } from '../../../model/query/promotion-new/index.js'
-import { utils } from '../../../utils/index.js'
 
 export async function findAll(query: PromotionNewsListQuery) {
     const { limit, next, status, date } = query
-    let qb = db
+    const qb = db
         .selectFrom('booking.promotion_new as pn')
         .selectAll()
         .where(eb => {
             const cond = []
             if (date) {
-                cond.push(eb('pn.startDate', '<=', date))
-                cond.push(eb('pn.endDate', '>=', date))
+                cond.push(
+                    eb.or([eb('pn.startDate', '<=', date), eb('pn.startDate', 'is', null)])
+                )
             }
             if (next) {
                 cond.push(eb('pn.id', '<', next))
