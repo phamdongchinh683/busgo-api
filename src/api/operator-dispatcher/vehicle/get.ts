ef7797854acb935b2c5c54e0b046369cfd45c218
pham dong chinh <dchinh6803@gmail.com>
2026-05-12 15:04:56 +0700

    update api get vehicle 12-5-2026

---
+++ src/api/operator-dispatcher/vehicle/get.ts
@@ -0,0 +1,29 @@
+import { api, endpoint, tags, bearer } from '../../../app/api.js'
+import { bus } from '../../../business/index.js'
+import { auth } from '../../../app/jwt/index.js'
+import { AuthUserRole } from '../../../database/auth/user/type.js'
+import { AuthStaffProfileRole } from '../../../database/auth/staff_profile/type.js'
+import { VehicleFilter } from '../../../model/query/vehicle/index.js'
+import { VehicleListResponse } from '../../../model/body/vehicle/index.js'
+
+const __filename = new URL('', import.meta.url).pathname
+
+api.route({
+    ...endpoint(__filename),
+
+    handler: async request => {
+        const userInfo = await auth.requireStaffProfileRole(
+            request.headers,
+            [AuthUserRole.enum.operator],
+            [AuthStaffProfileRole.enum.dispatcher]
+        )
+        return bus.organization.vehicle.getVehicles(request.query, userInfo.companyId)
+    },
+
+    schema: {
+        querystring: VehicleFilter,
+        response: { 200: VehicleListResponse },
+        tags: tags(__filename),
+        security: bearer,
+    },
+})
