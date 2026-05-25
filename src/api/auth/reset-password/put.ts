b7a56956a7abe161e147b580482e564d8c5163ec
pham dong chinh <dchinh6803@gmail.com>
2026-05-25 10:06:47 +0700

    update 25-5-2026

--- src/api/auth/reset-password/put.ts
+++ src/api/auth/reset-password/put.ts
@@ -7,12 +7,6 @@ const __filename = new URL('', import.meta.url).pathname
 
 api.route({
     ...endpoint(__filename),
-    config: {
-        rateLimit: {
-            max: 5,
-            timeWindow: '1m',
-        },
-    },
     handler: async request => {
         return bus.auth.password.resetPassword(request.body)
     },
--- src/api/auth/sign-in/post.ts
+++ src/api/auth/sign-in/post.ts
@@ -6,12 +6,6 @@ const __filename = new URL('', import.meta.url).pathname
 
 api.route({
     ...endpoint(__filename),
-    config: {
-        rateLimit: {
-            max: 5,
-            timeWindow: '30s',
-        },
-    },
     handler: async request => {
         return bus.auth.login.byEmailOrPhone(request.body)
     },
