bfa1102a659e184eb80eac6c8d8e1fb30fb86f19
pham dong chinh <dchinh6803@gmail.com>
2026-05-28 16:40:03 +0700

    update presigned upload file auth 28-5-2026

---
+++ src/api/file/upload/auth/presigned/get.ts
@@ -0,0 +1,22 @@
+import { api, endpoint, bearer, tags } from '../../../../../app/api.js'
+import { auth } from '../../../../../app/jwt/index.js'
+import { AuthUserRole } from '../../../../../database/auth/user/type.js'
+import { AuthStaffProfileRole } from '../../../../../database/auth/staff_profile/type.js'
+import { PresignedImageUploadResponse } from '../../../../../model/body/cloudinary/index.js'
+import { service } from '../../../../../service/index.js'
+
+const __filename = new URL('', import.meta.url).pathname
+
+api.route({
+    ...endpoint(__filename),
+
+    handler: async request => {
+        const userInfo = await auth.requiredAuthenticate(request.headers)
+        return service.cloudinary.presigned.presignedUpload('message', userInfo.companyId)
+    },
+    schema: {
+        response: { 200: PresignedImageUploadResponse },
+        tags: tags(__filename),
+        security: bearer,
+    },
+})
