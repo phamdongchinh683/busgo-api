ae27c5520cb7f55ec56c9fa21ff0320d4c9bf477
pham dong chinh <dchinh6803@gmail.com>
2026-05-28 16:45:27 +0700

    ...

--- src/api/file/upload/auth/presigned/get.ts
+++ src/api/file/upload/auth/presigned/get.ts
@@ -1,9 +1,8 @@
 import { api, endpoint, bearer, tags } from '../../../../../app/api.js'
 import { auth } from '../../../../../app/jwt/index.js'
-import { AuthUserRole } from '../../../../../database/auth/user/type.js'
-import { AuthStaffProfileRole } from '../../../../../database/auth/staff_profile/type.js'
 import { PresignedImageUploadResponse } from '../../../../../model/body/cloudinary/index.js'
 import { service } from '../../../../../service/index.js'
+import { PresignedImageUploadQuery } from '../../../../../model/query/presign/index.js'
 
 const __filename = new URL('', import.meta.url).pathname
 
@@ -11,10 +10,13 @@ api.route({
     ...endpoint(__filename),
 
     handler: async request => {
-        const userInfo = await auth.requiredAuthenticate(request.headers)
-        return service.cloudinary.presigned.presignedUpload('message', userInfo.companyId)
+        await auth.requiredAuthenticate(request.headers)
+
+        const { folder, id } = request.query
+        return service.cloudinary.presigned.presignedUpload(folder, id)
     },
     schema: {
+        querystring: PresignedImageUploadQuery,
         response: { 200: PresignedImageUploadResponse },
         tags: tags(__filename),
         security: bearer,
